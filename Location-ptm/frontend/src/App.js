543e8839d26a0161d1b883c6af124e2474702237
imoja021 <imoja021@student.wethinkcode.co.za>
2022-08-22 23:48:02 +0200

    changed map to work

--- Location-ptm/frontend/src/App.js
+++ Location-ptm/frontend/src/App.js
@@ -1,27 +1,30 @@
 import * as React from 'react';
-import Map from 'react-map-gl';
+import Map, {Marker} from 'react-map-gl';
+import {Room} from '@material-ui/icons';
 
 
 function App() {
-  // const [viewport, setViewport] = useState({
-  //   width: "100vw",
-  //   height: "100vh",
-  //   longitude: -122.4,
-  //   latitude: 37.8,
-  //   zoom: 8
-  // });
+
   return (
     <div className="App">
+    
      <Map
+
+      mapboxAccessToken = {process.env.REACT_APP_MAPBOX}
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
+        longitude: 34.5085,
+        latitude: 8.7832,
+        zoom: 5
       }}
       style={{width: "100vw", height: "100vh"}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX}
-    />
+      mapStyle="mapbox://styles/ipeleng21/cl759rp36001415lp7o28wdpp"
+      
+    >
+      <Marker longitude={31.0459} latitude={29.8673} anchor="bottom" >
+      <img src="./pin.png" alt = "pin"/>
+      <Room style={{fontSize: Marker.zoom *7 ,color: "red"}}/>
+    </Marker>
+    </Map>
     </div>
   );
 }
