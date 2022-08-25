fc172441c36a4dad930edcdbc26cfb1388294bf2
imoja021 <imoja021@student.wethinkcode.co.za>
2022-08-25 20:25:57 +0200

    code for the pop up has been written but does not work properly yet

--- Location-ptm/frontend/src/App.js
+++ Location-ptm/frontend/src/App.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import Map, {Marker} from 'react-map-gl';
-import {Room} from '@material-ui/icons';
-
+import Map, {Marker, Popup} from 'react-map-gl';
+import {Room, Star} from '@material-ui/icons';
+import "./app.css";
 
 function App() {
+  const [showPopup, setShowPopup] = React.useState(true);
 
   return (
     <div className="App">
@@ -20,10 +21,35 @@ function App() {
       mapStyle="mapbox://styles/ipeleng21/cl759rp36001415lp7o28wdpp"
       
     >
-      <Marker longitude={31.0459} latitude={29.8673} anchor="bottom" >
+      <Marker longitude={31.0459} latitude={29.8673} anchor = "bottom">
       <img src="./pin.png" alt = "pin"/>
       <Room style={{fontSize: Marker.zoom *7 ,color: "red"}}/>
     </Marker>
+
+    {/* {showPopup && (
+      <Popup longitude={31.0459} latitude={29.8673}
+        anchor="bottom"
+        onClose={() => setShowPopup(false)}>
+        <div className="card">
+        <label>Place</label>
+        <h4 className='place'>Somewhere in the world</h4>
+        <label>Review</label>
+        <p className='desc'>One spot I like going too</p>
+        <label>Rating</label>
+
+        <div className='stars'>
+        <Star className='stars'/>
+        <Star className='stars'/>
+        <Star className='stars'/>
+        <Star className='stars'/>
+        <Star className='stars'/>
+        </div>
+        
+        <label>Information</label>
+        <span className='username'>Created by <b>ipeleng</b></span>
+        <span className='date'>1 hour ago</span>
+        </div> 
+      </Popup>)}; */}
     </Map>
     </div>
   );
